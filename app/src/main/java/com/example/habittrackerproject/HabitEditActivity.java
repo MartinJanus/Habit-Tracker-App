dd1c0d3bec013c0a23a4b51c49e5ecd5f9b21bbe
Martin Janus <martinasjanuska@gmail.com>
2023-12-11 20:02:04 +0000

    Edit Start Date

--- app/src/main/java/com/example/habittrackerproject/HabitEditActivity.java
+++ app/src/main/java/com/example/habittrackerproject/HabitEditActivity.java
@@ -1,9 +1,11 @@
 package com.example.habittrackerproject;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -11,10 +13,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 
 public class HabitEditActivity extends AppCompatActivity {
     private EditText habitNameEdit;
     private EditText habitDescEdit;
+    private EditText startDateEdit;
     private Button saveChangesButton;
     private Habit habit;
     private ViewHabit viewHabit;
@@ -26,6 +33,7 @@ public class HabitEditActivity extends AppCompatActivity {
 
         habitNameEdit = findViewById(R.id.HabitNameEdit);
         habitDescEdit = findViewById(R.id.HabitDescEdit);
+        startDateEdit = findViewById(R.id.StartDateEdit);
         saveChangesButton = findViewById(R.id.buttonSaveChanges);
         viewHabit = new ViewModelProvider(this).get(ViewHabit.class);
 
@@ -43,15 +51,38 @@ public class HabitEditActivity extends AppCompatActivity {
                 });
             }
         }
+
+        startDateEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(HabitEditActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                                Calendar selectedDate = Calendar.getInstance();
+                                selectedDate.set(year, month, dayOfMonth);
+                                startDateEdit.setText(new SimpleDateFormat("dd-MMM-yyyy", Locale.getDefault()).format(selectedDate.getTime()));
+                            }
+                        }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
         saveChangesButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String updatedName = habitNameEdit.getText().toString();
                 String updatedDescription = habitDescEdit.getText().toString();
+                String updatedStartDate = startDateEdit.getText().toString();
 
                 if (habit != null) {
                     habit.setHabitName(updatedName);
                     habit.setHabitDescription(updatedDescription);
+                    habit.setStartDate(updatedStartDate);
                     viewHabit.update(habit);
                     Toast.makeText(HabitEditActivity.this, "Habit updated successfully", Toast.LENGTH_SHORT).show();
                 } else {
